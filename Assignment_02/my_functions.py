e93194bbf5b05eb6122ef67370e7a021c6185b7e
JoshuaEubanksUCF <joshua.eubanks@ucf.edu>
2025-01-29 06:23:08 -0500

    Update my_functions.py

--- Assignment_02/my_functions.py
+++ Assignment_02/my_functions.py
@@ -26,16 +26,13 @@ def present_value(cash_flow_amt, discount_rate: float, num_years) -> float:
     >>> present_value(150,.09, 12)
     53.33
     """
-    
+    # needs 3 examples (-1)
+    # needs either a precondition or checks to make sure that variables are not negative (-1)
     total = cash_flow_amt / ((1 + discount_rate) ** num_years)
     
     return total
 
-# Testing the function
-present_value(10,.03,6)
-# 8.37
-present_value(150,.09, 12)
-# 53.33
+
 
 def future_value(cash_flow_amt, discount_rate: float, num_years) -> float:
     """ Calculates the future value of a present cash flow
@@ -50,7 +47,8 @@ def future_value(cash_flow_amt, discount_rate: float, num_years) -> float:
     >>> present_value(150,.09, 12)
     53.33
     """
-    
+    # needs 3 examples (-1)
+    # needs either a precondition or checks to make sure that variables are not negative (-1)
     total = cash_flow_amt * ((1 + discount_rate) ** num_years)
     
     return total
@@ -70,6 +68,8 @@ def total_revenue(num_units, price: float) -> float:
     >>> total_revenue(8, 5.25)
     42.00
     """
+    # needs 3 examples (-1)
+    # needs either a precondition or checks to make sure that variables are not negative (-1)
     total = num_units * price
     
     return total
@@ -93,6 +93,8 @@ def total_cost(num_units, fixed_costs: float, cost_coeff: float) -> float:
     >>> total_cost(50,1300.0,2.9)
     8550.0
     """
+    # needs 3 examples (-1)
+    # needs either a precondition or checks to make sure that variables are not negative (-1)
     total = cost_coeff * num_units**2 + fixed_costs
     
     return total
@@ -119,6 +121,8 @@ def CESutility(consumed_good1, consumed_good2, degree_complements: float) -> flo
     >>> CESutility(170,168,-0.35)
     23.323
     """
+    # needs 3 examples (-1)
+    # needs either a precondition or checks to make sure that variables are not negative (-1)
     total = round((consumed_good1 ** degree_complements + consumed_good2 ** degree_complements)**(1/degree_complements),3)
     
     return total
@@ -130,3 +134,5 @@ CESutility(170,168,-0.35)
 # 23.323
 CESutility(1030,1600,0.9)
 # 2833.825
+
+# Missing the final part from the example script (-3)
