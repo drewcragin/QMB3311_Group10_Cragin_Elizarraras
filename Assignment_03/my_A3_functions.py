98564ba4125f13c29237da2e6dccb35e930860d2
JoshuaEubanksUCF <joshua.eubanks@ucf.edu>
2025-02-10 18:27:19 -0500

    Update my_A3_functions.py

--- Assignment_03/my_A3_functions.py
+++ Assignment_03/my_A3_functions.py
@@ -13,7 +13,22 @@
 #
 ##################################################
 """
-def CESutility_valid(consumed_good1, consumed_good2, degree_complements: float) -> float:
+
+def CESutility(good_x: float, good_y: float, r: float) -> float:
+    """Calculate the constant elasticity of subsitution utility function for two goods.
+    
+    >>> CESutility(3, 4, 2)
+    5.0
+    >>> CESutility(1, 1, 2)
+    1.4142135623730951
+    >>> CESutility(3**0.5, 4**0.5, 4)
+    2.23606797749979
+    """
+    
+    utility = (good_x**r + good_y**r)**(1/r)
+    return utility
+
+def CESutility_valid(consumed_good1, consumed_good2, degree_complements: float) -> float: # expected input for the goods? (-1)
     """ Calculates value of Constant Elasticity of Substitution utility function
    which measures degree of satisfaction a consumer gets from two goods with formula
   (x^r+y^r)^1/r while 
@@ -24,7 +39,7 @@ def CESutility_valid(consumed_good1, consumed_good2, degree_complements: float) 
 
    >>> CESutility_valid(5, 8, 3)
    8.604
-   >>> CESutility_valid(-5, 8, 3)
+   >>> CESutility_valid(-5, -8, 3)
    None
    >>> CESutility_valid(5, -8, 3)
    None
@@ -41,21 +56,11 @@ def CESutility_valid(consumed_good1, consumed_good2, degree_complements: float) 
         print ("r must be strictly positive")
         return None
     
-    total = round((consumed_good1 ** degree_complements + consumed_good2 ** degree_complements)**(1/degree_complements),3)
+    total = CESutility(consumed_good1, consumed_good2, degree_complements)
    
     return total
-
-    # Testing the function
-    CESutility_valid(5, 8, 3)
-    # 8.604
-    CESutility_valid(-5, 8, 3)
-    # Error message for x
-    CESutility_valid(5, -8, 3)
-    # Error message for y
-    CESutility_valid(5, 8, -3)
-    # Error message for r
     
-    def CESutility_in_budget(consumed_good1, consumed_good2, degree_complements: float, p_x, p_y, w) -> float:
+def CESutility_in_budget(consumed_good1, consumed_good2, degree_complements: float, p_x, p_y, w) -> float: # unexpected indent (-1)
         """ Evaluates the CESutility_valid function when the consumer's choice of goods
         x and y are in budget, and returns None otherwise using the wealth formula
         w >= p_x*x + p_y*y
@@ -89,18 +94,10 @@ def CESutility_valid(consumed_good1, consumed_good2, degree_complements: float) 
         
         return CESutility_valid(consumed_good1, consumed_good2, degree_complements)
         
-        # Testing the function
-        CESutility_in_budget(5, 3, 2, 2, 2, 20)
-        # 5.831
-        CESutility_in_budget(5, 3, 2, 2, 2, 4)
-        # Error message for wealth
-        CESutility_in_budget(5, 3, -2, 2, 2, 20)
-        # Error message for r
-        CESutility_in_budget(5, 3, 2, -2, 2, 20)
-        # Error message for p_x
 
 import math
-def logit(x,beta0,beta1):
+
+def logit(x,beta0,beta1): # expected input/output (-2)
     """calculates the logit link function, which gives the probability of a binary result [prob(y=1|x)] in a form that can be 
     modelled linearly. e^b0+xb1 / 1+e^ b0+xb1
 
@@ -119,47 +116,78 @@ def logit(x,beta0,beta1):
     
     return total
 
-    # Testing the function    
-    logit(1,0,math.log(1))
-    # 0.5
-    logit(1,0,math.log(9))
-    # 0.9
-    logit(1,0,math.log(3))
-    # 0.75
+# Testing the function    
+logit(1,0,math.log(1))
+# 0.5
+logit(1,0,math.log(9))
+# 0.9
+logit(1,0,math.log(3))
+# 0.75
     
-    def logit_like(y_i, x_i, beta0, beta1):
-        """ Calculates the log-likelihood of observation (y_i;x_i)
-        by returning the log of the function (x;b_0,b_1) if y_i = 1
-        and the log of the function 1-(x;b_0,b_1) if y_i = 0
-        
-        y_i - Binary variable for if event occured
-        x_i - Independent variable
-        beta0 - Beta 0, the intercept
-        beta1 - Beta 1, the slope
-        
-        >>> logit_like(1, 1, 0, math.log(3))
-        
-        >>> logit_like(0, 1, 0, math.log(3))
-        
-        >>> logit_like(5, 1, 0, math.log(3))
-        Error message for y_i
 
-        """
-        probability = logit(x_i, beta0, beta1)
-        
-        if y_i == 1:
-            return math.log(probability)
-        if y_i == 0:
-            return math.log(1-probability)
-        else:
-            print("y_i must be 1 or 0")
-            return None
-        
-        # Testing the function
-        logit_like(1, 1, 0, math.log(3))
-        
-        logit_like(0, 1, 0, math.log(3))
-        
-        logit_like(5, 1, 0, math.log(3))
+def logit_like(y_i, x_i, beta0, beta1): # missing expected input/output (-2)
+# indention issues (-1)
+    """ Calculates the log-likelihood of observation (y_i;x_i)
+    by returning the log of the function (x;b_0,b_1) if y_i = 1
+    and the log of the function 1-(x;b_0,b_1) if y_i = 0
+  
+    y_i - Binary variable for if event occured
+    x_i - Independent variable
+    beta0 - Beta 0, the intercept
+    beta1 - Beta 1, the slope
+       
+    >>> logit_like(1, 1, 0, math.log(3))
+    
+    >>> logit_like(0, 1, 0, math.log(3))
+       
+    >>> logit_like(5, 1, 0, math.log(3))
+    Error message for y_i
+
+    """
+    # missing expected return values for first two examples (-1)
+    probability = logit(x_i, beta0, beta1)
+       
+    if y_i == 1:
+        return math.log(probability)
+    elif y_i == 0:
+        return math.log(1-probability)
+    else:
+        print("y_i must be 1 or 0")
+        return None
+    
+# Testing the function
+CESutility_valid(5, 8, 3)
+# 8.604
+CESutility_valid(-5, 8, 3)
+# Error message for x
+CESutility_valid(5, -8, 3)
+# Error message for y
+CESutility_valid(5, 8, -3)
+# Error message for r
+   
+# Testing the function
+CESutility_in_budget(5, 3, 2, 2, 2, 20)
+# 5.831
+CESutility_in_budget(5, 3, 2, 2, 2, 4)
+# Error message for wealth
+CESutility_in_budget(5, 3, -2, 2, 2, 20)
+# Error message for r
+CESutility_in_budget(5, 3, 2, -2, 2, 20)
+# Error message for p_x
+
+# Testing the function    
+logit(1,0,math.log(1))
+# 0.5
+logit(1,0,math.log(9))
+# 0.9
+logit(1,0,math.log(3))
+# 0.75
+
+  # Testing the function
+logit_like(1, 1, 0, math.log(3))
+    
+logit_like(0, 1, 0, math.log(3))
+      
+logit_like(5, 1, 0, math.log(3))
         # Error message for y_i
       
